e078b5a603f8454ab68c69f7e11c0ec3b66ae2aa
jay274 <jay@devineglobe.com>
2026-03-27 17:57:45 +0530

    title change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zonalta - Admin Login</title>
+    <title>Welbourg Sakhi Day - Admin Login</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+
     <!-- Changed font to Nunito to match index.blade.php -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
